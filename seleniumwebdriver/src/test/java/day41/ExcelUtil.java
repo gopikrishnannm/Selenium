ea1e75c800abba959e4f4a3e35359fe8b36b70e4
gopikrishnannm <gopikrishnannm01@gmail.com>
2024-09-02 18:44:46 +0530

    41 half

---
+++ seleniumwebdriver/src/test/java/day41/ExcelUtil.java
@@ -0,0 +1,93 @@
+package day41;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtil {
+	
+	public static FileInputStream fi;
+	public static FileOutputStream fo;
+	public static XSSFWorkbook wb;
+	public static XSSFSheet ws;
+	public static XSSFRow row;
+	public static XSSFCell cell;
+	public static CellStyle style;
+	
+	public static int getRowCount(String xlfile, String xlsheet) throws IOException {
+		
+		fi = new FileInputStream(xlfile);
+		wb = new XSSFWorkbook(fi);
+		ws = wb.getSheet(xlsheet);
+		int rowCount = ws.getLastRowNum();
+		wb.close();
+		fi.close();
+		return rowCount;
+	}
+	
+	public static int getCellCount(String xlfile, String xlsheet, int rownum) throws IOException {
+		
+		fi = new FileInputStream(xlfile);
+		wb = new XSSFWorkbook(fi);
+		ws = wb.getSheet(xlsheet);
+		row = ws.getRow(rownum);
+		int cellCount = row.getLastCellNum();
+		wb.close();
+		fi.close();
+		return cellCount;
+	}
+	public static String getCellData(String xlfile, String xlsheet, int rownum, int colnum) throws IOException {
+		
+		fi = new FileInputStream(xlfile);
+		wb = new XSSFWorkbook(fi);
+		ws = wb.getSheet(xlsheet);
+		row = ws.getRow(rownum);
+		cell = row.getCell(colnum);
+		String data;
+		try {
+			//data = cell.toString();
+			DataFormatter formatter = new DataFormatter();
+			data = formatter.formatCellValue(cell);
+		}
+		catch(Exception e) {
+			data = "";
+		}
+		finally {
+			wb.close();
+			fi.close();
+		}
+		return data;
+	}
+	public static void setCellData(String xlfile, String xlsheet, int rownum, int colnum, String data) throws IOException {
+		
+		fi = new FileInputStream(xlfile);
+		wb = new XSSFWorkbook(fi);
+		ws = wb.getSheet(xlsheet);
+		/* 
+		 The XSSFWorkbook class, which represents the 
+		 workbook in memory, contains methods for working 
+		 with sheets, such as getSheet and createSheet, 
+		 regardless of whether you're using FileInputStream 
+		 (fi) or FileOutputStream (fo) to interact with the file
+
+		 */
+		row = ws.getRow(rownum);
+		cell = row.getCell(colnum);
+		cell.setCellValue(data);
+		fo = new FileOutputStream(xlfile);
+		wb.write(fo);
+		wb.close();
+		fi.close();
+		fo.close();
+		
+	}
+
+	
+}
